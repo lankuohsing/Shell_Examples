47d4743038e6efae96478c6cd23a968d7ea51c76
lankuohsing <lankuohsing@163.com>
2021-08-15 20:10:40 +0800

    add copy files example

--- copy_files_from_source_to_target_dir/copy_all_files.sh
+++ copy_files_from_source_to_target_dir/copy_all_files.sh
@@ -12,7 +12,7 @@ function getdir(){
         else
             ori_file=$dir_or_file
             if [ "${ori_file##*.}"x = "wav"x ]; then
-                new_file=$2 "${ori_file#*$1}"
+                new_file=$2"${ori_file#*$1}"
                 echo $ori_file
                 echo $new_file
                 cp $ori_file $new_file
